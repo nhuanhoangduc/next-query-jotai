c9c04c14b2826e55a1d2ed60b950f7e6311594a9
nhuanhoangduc <nhuanhoangduc>
2024-07-16 22:35:51 +0700

    [skip ci] bk

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import { createStore, Provider } from "jotai";
+
+const myStore = createStore();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={myStore}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
