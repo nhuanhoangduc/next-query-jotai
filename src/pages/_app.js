601b806e73c9c0a9a7ab05abd2c338eed9c620cf
nhuanhoangduc <nhuanhoangduc>
2024-07-17 01:13:07 +0700

    [skip ci] bk

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { createStore, Provider } from "jotai";
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 const myStore = createStore();
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <Provider store={myStore}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps?.dehydratedQueryState}>
+        <Provider store={myStore}>
+          <Component {...pageProps} />
+        </Provider>
+      </HydrationBoundary>
+    </QueryClientProvider>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,29 @@ import Head from "next/head";
 import { atom, useAtomValue } from "jotai";
 import { atomFamily } from "jotai/utils";
 import _ from "lodash";
+import { useQuery } from "@tanstack/react-query";
 
-const PostState = atomFamily(
-  (_id) => atom({ title: "iOS 18 could ‘sherlock’ $400M in app revenue" }),
-  _.isEqual
-);
+// state
+// const PostState = atomFamily(({ _id }) => atom(), _.isEqual);
+
+// query
+const queryFn = ({ postId }) =>
+  fetch("https://jsonplaceholder.typicode.com/posts/" + postId).then(
+    (response) => response.json()
+  );
+const queryKey = ["getPosts"];
+const useGetPosts = (queryParams) => {
+  const query = useQuery({
+    queryKey: queryKey,
+    queryFn: () => queryFn(queryParams),
+  });
+  return query;
+};
 
 export default function Home() {
-  const post = useAtomValue(PostState(1));
+  const { data: post, isLoading } = useGetPosts({ postId: 1 });
 
+  if (isLoading) return "Loading....";
   return (
     <>
       <Head>
@@ -18,6 +32,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>{post.title}</h1>
+        <p>{post.body}</p>
       </main>
     </>
   );
